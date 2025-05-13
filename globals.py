17ea151afeedf76f1961824bb5976336bb0e1349
andreaforster <97340144+chicadelica@users.noreply.github.com>
2025-05-14 01:34:11 +0200

    bug fix wrong author url link generation

--- globals.py
+++ globals.py
@@ -5,7 +5,6 @@ available_datasets = [
     "yelp",
 ]  # choose betweeen "yelp", "gowalla", "foursquaretky", and "brightkite" and make sure to add the datasets to your BASE_DIR
 
-
 BASE_DIR = "./datasets/"
 
 # wikimedia_headers = CREATE YOUR OWN WIKIMEDIA API TOKEN
